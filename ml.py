24d8615a7f49a3d19212f220a86de66f764773db
markf <markfitz815@gmail.com>
2022-11-13 20:39:06 +0000

    methods for standard deviation and variance

--- ml.py
+++ ml.py
@@ -6,7 +6,11 @@ speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 mean = np.mean(speed)
 median = np.median(speed)
 mode = stats.mode(speed)
+standardDeviation = np.std(speed)
+variance = np.var(speed)
 
 print(mean, 'mean')
 print(median, 'median')
 print(mode, 'mode')
+print(standardDeviation, 'standardDeviation')
+print(variance, 'variance')
